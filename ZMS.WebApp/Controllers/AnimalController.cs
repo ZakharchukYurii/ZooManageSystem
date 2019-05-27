178903cdcd7c7a33ddedca8ab7caae9a59ede31a
Yurii Zakharchuk <yurii_zakharchuk@epam.com>
2019-05-27 09:19:56 +0300

    Add WebApp layer to project

---
+++ ZMS.WebApp/Controllers/AnimalController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZMS.BLL.Abstracts;
+
+namespace ZMS.WebApp.Controllers
+{
+    [Route("api/Animal")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        IAnimalService _service;
+
+        public AnimalController(IAnimalService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPut]
+        public IActionResult Feed()
+        {
+            _service.Update(id, item);
+            return Ok();
+        }
+
+    }
+}
