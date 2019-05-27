7923975d8bfd2f9c61f7ba7931562581679bc06f
yzakharchuk <yzakharchuk@foundationmedicine.com>
2019-05-27 11:58:33 +0300

    Implement AnimalService

--- ZMS.BLL/Abstracts/IAnimalService.cs
+++ ZMS.BLL/Abstracts/IAnimalService.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ZMS.BLL.DTO;
 
 namespace ZMS.BLL.Abstracts
 {
-    public interface IAnimalService
+    public interface IAnimalService : IDisposable
     {
+        AnimalDTO Get(int id);
         IEnumerable<AnimalDTO> GetAll();
+        void AddNew(AnimalDTO item);
         void Update(int id, AnimalDTO item);
     }
 }
--- ZMS.BLL/Services/AnimalService.cs
+++ ZMS.BLL/Services/AnimalService.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AutoMapper;
 using ZMS.BLL.Abstracts;
 using ZMS.BLL.DTO;
+using ZMS.BLL.Infrastructure;
 using ZMS.DAL.Abstracts;
 using ZMS.DAL.Entities;
 
@@ -9,32 +12,67 @@ namespace ZMS.BLL.Services
 {
     public class AnimalService : IAnimalService
     {
-        private IUnitOfWork _unitOfWork;
+        private IUnitOfWork _database;
         private IMapper _mapper;
 
         public AnimalService(IUnitOfWork uow)
         {
-            _unitOfWork = uow;
+            _database = uow;
             _mapper = new MapperConfiguration(cfg => cfg.CreateMap<AnimalDTO, Animal>().ReverseMap()).CreateMapper();
         }
 
         public AnimalDTO Get(int id)
         {
-            var result = _unitOfWork.Animals.Get(id);
+            var result = _database.Animals.Get(id);
 
             return _mapper.Map<AnimalDTO>(result);
         }
 
         public IEnumerable<AnimalDTO> GetAll()
         {
-            var result = _unitOfWork.Animals.GetAll();
+            var result = _database.Animals.GetAll();
 
             return _mapper.Map<IEnumerable<AnimalDTO>>(result);
         }
 
+        public void AddNew(AnimalDTO item)
+        {
+            if (item == null)
+            {
+                throw new ValidationException("Item is undefined");
+            }
+
+            var newItem = _mapper.Map<Animal>(item);
+
+            _database.Animals.Create(newItem);
+            _database.Save();
+        }
+
+
         public void Update(int id, AnimalDTO item)
         {
-            throw new System.NotImplementedException();
+            if (id < 1)
+            {
+                throw new Exception("Id is not valid");
+            }
+
+            var itemToUpdate = _database.Animals.Get(id);
+
+            itemToUpdate.Name = item.Name;
+            itemToUpdate.Age = item.Age;
+
+            if (item.Food.FirstOrDefault() != null)
+            {
+                (itemToUpdate.Food as List<string>).AddRange(item.Food);
+            }
+
+            _database.Animals.Update(itemToUpdate);
+            _database.Save();
+        }
+
+        public void Dispose()
+        {
+            _database.Dispose();
         }
     }
 }
