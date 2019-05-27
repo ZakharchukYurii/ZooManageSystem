fa440664b19b526849ce1382722111f12d672a34
Yurii Zakharchuk <yurii_zakharchuk@epam.com>
2019-05-28 02:52:22 +0300

    Add new exception type NullDataException and modify ValidationException

---
+++ ZMS.BLL/Infrastructure/NullDataException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ZMS.BLL.Infrastructure
+{
+    public class NullDataException : Exception
+    {
+        public NullDataException() { }
+        public NullDataException(string message) : base(message) { }
+        public NullDataException(string message, Exception inner) : base(message, inner) { }
+        protected NullDataException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
--- ZMS.BLL/Infrastructure/ValidationException.cs
+++ ZMS.BLL/Infrastructure/ValidationException.cs
@@ -4,13 +4,11 @@ namespace ZMS.BLL.Infrastructure
 {
     public class ValidationException : Exception
     {
-        public ValidationException(string message)
-            : base(message)
-        { }
-
-        public ValidationException(string message, Exception inner)
-            : base(message, inner)
-        { }
+        public ValidationException() { }
+        public ValidationException(string message) : base(message) { }
+        public ValidationException(string message, Exception inner) : base(message, inner) { }
+        protected ValidationException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
-
 }
