f6b21a958b25d57ccdfddcb8828abdf27af64488
yzakharchuk <yzakharchuk@foundationmedicine.com>
2019-05-31 18:13:11 +0300

    Tech commit with some edits

--- ZMS.BLL/Services/AnimalService.cs
+++ ZMS.BLL/Services/AnimalService.cs
@@ -23,16 +23,9 @@ namespace ZMS.BLL.Services
         public AnimalDTO Get(int id)
         {
             if (id < 1)
-            {
                 throw new ValidationException("Id is not valid");
-            }
 
             var result = _database.Animals.Get(id);
-
-            if(result == null)
-            {
-                throw new NullDataException("Item not found");
-            }
 
             return _mapper.Map<AnimalDTO>(result);
         }
@@ -40,12 +33,7 @@ namespace ZMS.BLL.Services
         public IEnumerable<AnimalDTO> GetAll()
         {
             var result = _database.Animals.GetAll();
-
-            if (result == null)
-            {
-                throw new NullDataException("Items does not exist");
-            }
-
+            
             return _mapper.Map<IEnumerable<AnimalDTO>>(result);
         }
 
@@ -64,17 +52,7 @@ namespace ZMS.BLL.Services
 
         public void Update(int id, AnimalDTO item)
         {
-            if (id < 1)
-            {
-                throw new ValidationException("Id is not valid");
-            }
-
             var itemToUpdate = _database.Animals.Get(id);
-
-            if (itemToUpdate == null)
-            {
-                throw new NullDataException("Animal does not exist");
-            }
 
             itemToUpdate.Name = item.Name;
             itemToUpdate.IsHungry = item.IsHungry;
@@ -129,28 +107,8 @@ namespace ZMS.BLL.Services
 
         public void AttachCaretaker(int animalId, int caretakerId)
         {
-            if (animalId < 1)
-            {
-                throw new ValidationException("AnimalId is not valid");
-            }
-            if (caretakerId < 1)
-            {
-                throw new ValidationException("CaretakerId is not valid");
-            }
-
             var animal = _database.Animals.Get(animalId);
-
-            if (animal == null)
-            {
-                throw new NullDataException("Animal does not exist");
-            }
-
             var caretaker = _database.Employees.Get(caretakerId);
-
-            if (caretaker == null)
-            {
-                throw new NullDataException("Caretaker does not exist");
-            }
 
             animal.CaretakerId = caretaker.Id;
 
