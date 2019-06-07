b30cda0ed1906771a27e2703b93b70759564af75
Yurii Zakharchuk <yurii_zakharchuk@epam.com>
2019-06-07 09:48:58 +0300

    Edit animal entity to use nullable type

--- ZMS.BLL/DTO/AnimalDTO.cs
+++ ZMS.BLL/DTO/AnimalDTO.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ZMS.BLL.DTO
+﻿namespace ZMS.BLL.DTO
 {
     public class AnimalDTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
-        private int _age;
-        public int Age
-        {
-            get => _age;
-            set => _age = value >= 0 ? value : 0;
-        }
-
+        public int? Age { get; set; }
         public Sex Sex { get; set; }
         public bool IsHungry { get; set; }
 
--- ZMS.DAL/Entities/Animal.cs
+++ ZMS.DAL/Entities/Animal.cs
@@ -4,14 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
-        private int _age;
-        public int Age
-        {
-            get => _age;
-            set => _age = value >= 0 ? value : 0;
-        }
-
+        public int? Age { get; set; }
         public Sex Sex { get; set; }
         public bool IsHungry { get; set; }
 
